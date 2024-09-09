0e69bbcc39a48000ff594a03d02347e754d7ead6
Shrijit Sengupta <142336020+Cyprohacker@users.noreply.github.com>
2024-09-09 09:57:08 +0530

    Delete NwPort directory

--- NwPort/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text",{
-    strings:["Frontend Designer","web Developer","Content Creator "],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true,
-})
